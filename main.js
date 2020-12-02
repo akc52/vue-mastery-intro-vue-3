3c96d381b66a6ae53000ed65f9c22aa0a066d41b
akc52 <alyssa.ciccone@bounteous.com>
2020-12-02 15:05:12 -0500

    Conditional rendering

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Sold in non-smelly',
+            description: 'Sold in non-smelly option',
             image: './assets/images/socks_green.jpg',
+            inventory: 9,
             url: 'https://www.google.ca'
         }
     }
