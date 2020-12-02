ad6e02cea3ff5d369af03de5485d770fe0ed945d
akc52 <alyssa.ciccone@bounteous.com>
2020-12-02 17:17:44 -0500

    Event Handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'Sold in non-smelly option',
             image: './assets/images/socks_green.jpg',
@@ -8,10 +9,21 @@ const app = Vue.createApp({
             url: 'https://www.google.ca',
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id:2234, color: 'blue' },
-                { id:2235, color: 'green' }
+                { id:2234, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id:2235, color: 'green', image: './assets/images/socks_green.jpg' }
             ],
             sizes: [ 'small', 'large', 'extra-large' ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            this.cart -= 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
