ff7582531dd2e1ce029b4799a912ed6f383383ad
akc52 <alyssa.ciccone@bounteous.com>
2020-12-02 14:47:59 -0500

    Attribute binding

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Sold in non-smelly'
+            description: 'Sold in non-smelly',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.google.ca'
         }
     }
 })
