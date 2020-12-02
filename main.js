162b7bd4b4844a46c587ec6ba64aaa37a81db3ad
akc52 <alyssa.ciccone@bounteous.com>
2020-12-02 17:07:03 -0500

    List rendering

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ const app = Vue.createApp({
             description: 'Sold in non-smelly option',
             image: './assets/images/socks_green.jpg',
             inventory: 9,
-            url: 'https://www.google.ca'
+            url: 'https://www.google.ca',
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id:2234, color: 'blue' },
+                { id:2235, color: 'green' }
+            ],
+            sizes: [ 'small', 'large', 'extra-large' ]
         }
     }
 })
