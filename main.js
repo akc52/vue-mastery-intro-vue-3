8a55631f7ad65685e3accd4234b860a944a6a9c1
akc52 <alyssa.ciccone@bounteous.com>
2020-12-03 15:52:44 -0500

    Computed Properties

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const app = Vue.createApp({
             cart: 0,
             product: 'Socks',
             description: 'Sold in non-smelly option',
-            image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
+            brand: 'Awesome Sauce',
             activeClass: true,
-            inStock: false,
             inventory: 9,
             url: 'https://www.google.ca',
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id:2234, color: 'blue', image: './assets/images/socks_blue.jpg' },
-                { id:2235, color: 'green', image: './assets/images/socks_green.jpg' }
+                { id:2234, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 50 },
+                { id:2235, color: 'green', image: './assets/images/socks_green.jpg', quantity: 0 }
             ],
             sizes: [ 'small', 'large', 'extra-large' ]
         }
@@ -24,8 +24,21 @@ const app = Vue.createApp({
         removeFromCart() {
             this.cart -= 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        //no longer data property, but computed property
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        }
+
     }
 })
