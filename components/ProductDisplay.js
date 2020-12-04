eb0ec2313e199aba668d5cbb34f3f90c1d8c5298
akc52 <alyssa.ciccone@bounteous.com>
2020-12-04 17:29:08 -0500

    Small adjustments to style

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -21,26 +21,30 @@ app.component('product-display', {
             <p v-if="inventory > 10">In Stock</p>
             <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
             <p v-else>Out of Stock</p>
-            <p>{{ shipping }} Shipping</p>
-            <button 
-            class="button" 
-            :class="{ disabledButton: !inStock }"
-            v-on:click="addToCart" 
-            :disabled="!inStock">
-                Add to Cart
-            </button>
-            <button 
-            class="button" 
-            v-on:click="removeById">
-                Remove
-            </button>
-            <div 
-            v-for="(variant, index) in variants" 
-            v-bind:key="variant.id" 
-            @mouseover="updateVariant(index)" 
-            class="color-circle"
-            :class="{ active: activeClass }"
-            :style="{backgroundColor: variant.color }"></div>
+            <p class="shipping">{{ shipping }} Shipping</p>
+            <div class="product-variants">
+                <div 
+                v-for="(variant, index) in variants" 
+                v-bind:key="variant.id" 
+                @mouseover="updateVariant(index)" 
+                class="color-circle"
+                :class="{ active: activeClass }"
+                :style="{backgroundColor: variant.color }"></div>
+            </div>
+            <div class="product-buttons">
+                <button 
+                class="button" 
+                :class="{ disabledButton: !inStock }"
+                v-on:click="addToCart" 
+                :disabled="!inStock">
+                    Add to Cart
+                </button>
+                <button 
+                class="button" 
+                v-on:click="removeById">
+                    Remove
+                </button>
+            </div>
         </div>
         <review-list v-if="reviews.length" :reviews="reviews"></review-list>
         <review-form @review-submitted="addReview"></review-form>
@@ -55,7 +59,6 @@ app.component('product-display', {
         brand: 'Awesome Sauce',
         activeClass: true,
         inventory: 9,
-        url: 'https://www.google.ca',
         details: ['50% cotton', '30% wool', '20% polyester'],
         variants: [
             { id:2234, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 50 },
@@ -67,11 +70,9 @@ app.component('product-display', {
     },
     methods: {
         addToCart() {
-            //this.cart += 1
             this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
         },
         removeById(id) {
-            //this.cart -= 1
             this.$emit('remove-by-id', this.variants[this.selectedVariant].id)
         },
         updateVariant(index) {
@@ -88,7 +89,6 @@ app.component('product-display', {
         image(){
             return this.variants[this.selectedVariant].image
         },
-        //no longer data property, but computed property
         inStock(){
             return this.variants[this.selectedVariant].quantity
         },
@@ -97,8 +97,7 @@ app.component('product-display', {
                 return 'Free'
             } else {
                 return '$99'
-            }
-            
+            }  
         }
 
     }
