675afb74d8b1cb5114d41b20b18ea1ebcf17438f
akc52 <alyssa.ciccone@bounteous.com>
2020-12-04 11:09:42 -0500

    Components and Props

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -7,55 +7,42 @@ app.component('product-display', {
     },
     template:
     /* html */
-    `      <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <img 
-          :src="image" 
-          :alt="description"
-          :class="{ 'out-of-stock-img': !inStock }" />
-      </div>
-      <div class="product-info">
-        <h1>{{ title }}</h1>
-        <p v-if="inventory > 10">In Stock</p>
-        <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
-        <p v-else>Out of Stock</p>
-        <p>{{ shipping }} Shipping</p>
-        <button 
-          class="button" 
-          :class="{ disabledButton: !inStock }"
-          v-on:click="addToCart" 
-          :disabled="!inStock">
-            Add to Cart
-        </button>
-        <button 
-          v-if="cart > 0" 
-          class="button" 
-          v-on:click="removeFromCart">
-            Remove One
-          </button>
-        <p>{{ description }}</p>
-        <div 
-          v-for="(variant, index) in variants" 
-          v-bind:key="variant.id" 
-          @mouseover="updateVariant(index)" 
-          class="color-circle"
-          :class="{ active: activeClass }"
-          :style="{backgroundColor: variant.color }"></div>
-        <figure>
-          <figcaption>Details:</figcaption>
-          <ul>
-            <li v-for="detail in details">{{ detail }}</li>
-          </ul>
-        </figure>
-        <figure>
-          <figcaption>Available in these sizes:</figcaption>
-          <ul>
-            <li v-for="size in sizes">{{ size }}</li>
-          </ul>
-        </figure>
-        <a :href="url">Learn More!</a>
-    </div>
+    `
+    <div class="product-display">
+        <div class="product-container">
+        <div class="product-image">
+            <img 
+            :src="image" 
+            :alt="description"
+            :class="{ 'out-of-stock-img': !inStock }" />
+        </div>
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inventory > 10">In Stock</p>
+            <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+            <p v-else>Out of Stock</p>
+            <p>{{ shipping }} Shipping</p>
+            <button 
+            class="button" 
+            :class="{ disabledButton: !inStock }"
+            v-on:click="addToCart" 
+            :disabled="!inStock">
+                Add to Cart
+            </button>
+            <button 
+            v-if="cart > 0" 
+            class="button" 
+            v-on:click="removeFromCart">
+                Remove One
+            </button>
+            <div 
+            v-for="(variant, index) in variants" 
+            v-bind:key="variant.id" 
+            @mouseover="updateVariant(index)" 
+            class="color-circle"
+            :class="{ active: activeClass }"
+            :style="{backgroundColor: variant.color }"></div>
+        </div>
     </div>
   </div>`,
   data() {
